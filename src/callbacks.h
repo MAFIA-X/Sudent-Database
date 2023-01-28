d4df2e8c8539fb00dcbac6ad8fff9c4be197b463
MAFIA-X <jenbsubhrajit@gmail.com>
2023-01-28 11:15:33 +0530

    initial commit

---
+++ src/callbacks.h
@@ -0,0 +1,30 @@
+#include <gtk/gtk.h>
+
+
+void
+on_Add_clicked                         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Delete_clicked                      (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Edit_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Search_clicked                      (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Exit_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Next_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Prev_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
